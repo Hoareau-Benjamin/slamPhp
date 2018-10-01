c064c88ecc0abcfe5633724e4e028c4de62f276e
Hoareau Benjamin <43531479+Hoareau-Benjamin@users.noreply.github.com>
2018-10-01 11:54:32 +0400

    Add files via upload
    poo objet

---
+++ 2_Annuaire_Etd_poo/view/employe/add.php
@@ -0,0 +1,46 @@
+
+
+
+
+
+<?php include "templates/header.php";?>
+
+<h2>Adjouter un employé</h2>
+<a href="?ctrl=employe">Retour</a><br><br>
+<form action="?ctrl=employe&mth=add" method="post">
+    <label for="prenom">Prénom</label><br>
+    <input type="text" name="prenom" id="prenom"><br>
+    <label for="nom">Nom</label><br>
+    <input type="text" name="nom" id="nom"><br>
+    <label for="email">Adresse mail</label><br>
+    <input type="text" name="email" id="email"><br>
+    <label for="age">Age</label><br>
+    <input type="text" name="age" id="age"><br>
+    <label for="ville">ville de résidence</label><br>
+    <input type="text" name="ville" id="ville">
+    <label for="pseudo">pseudo</label><br>
+    <input type="text" name="pseudo" id="pseudo"><br>
+    <label for="mdp">Mot de passe</label><br>
+    <input type="text" name="mdp" id="mdp"><br>
+    <label for="">Prénom</label><br>
+    <input type="text" name="prenom" id="prenom"><br>
+    <label for="prenom">Prénom</label><br>
+    <input type="text" name="prenom" id="prenom"><br>
+    <label for="prenom">Prénom</label><br>
+    <input type="text" name="prenom" id="prenom"><br>
+    <br><br>
+    <input type="submit" name="submit" value="Ajouter">
+    <br><br>
+
+	<?php if ($errors) {
+			?>
+			<h3>Erreur:</h3>
+			<ul>
+				<?php foreach ($errors as $value) { ?>
+					<li><?php echo $value; ?></li>
+				<?php } ?>
+			</ul>
+	<?php } ?>
+</form>
+
+<?php include "templates/footer.php";?>
